97ef12748a18eec796e99003408465fc29cfeebf
x1frm <alex-shvets@yandex.ru>
2019-02-25 16:00:32 +0300

    node packages installed locally

--- webpack.config.js
+++ webpack.config.js
@@ -19,27 +19,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
       }
     ]
   }
